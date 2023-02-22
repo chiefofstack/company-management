580b9a02d60932bf2a2405ee4ed9d346a3a09285
chiefofstack <chiefofstack@gmail.com>
2023-02-22 15:05:22 +0000

    updated the company model, + added request validation on the store method in the company controller

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Company;
+
+class CompanyController extends Controller
+{
+    public function index(){
+    
+        $companies = Company::all();
+    
+        return view('companies.index', compact('companies'));
+    }
+    public function show(){
+    
+        $companies = Company::all();
+    
+        return view('companies.index', compact('companies'));
+    }
+
+    public function store(){
+        //validate
+        $attributes = request()->validate([
+            'name' => ['required','max:255'],
+            'email' => ['required','max:255'],
+            'logo' => ['required','max:255'],
+            'website' => ['required','max:255']
+            // 'name' => ['required','max:255'],
+            // 'email' => ['nullable','email:rfc,dns','max:255'],
+            // 'logo' => ['nullable','image','dimensions:min_width=100,min_height=100','max:1024'],
+            // 'website' => ['nullable','url','max:255']
+        ]);
+
+        //persist
+        Company::create($attributes);
+
+        //redirect
+        return redirect('/companies');
+    }
+}
