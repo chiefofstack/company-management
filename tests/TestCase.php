aaa6a675bdc2461657427e3797d6b3b8b6f6df94
chiefofstack <chiefofstack@gmail.com>
2023-02-23 14:34:59 +0000

    added a sign in method for presudo login use in the tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    //pseudo sign in
+    protected function signIn($user = null) 
+    {
+        $user = $user ?: factory('App\User')->create(); //if user is not defined create one
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
