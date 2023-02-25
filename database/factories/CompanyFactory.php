e15f42432fc46b3b83458737f7df5a77a3866d83
chiefofstack <chiefofstack@gmail.com>
2023-02-25 15:47:26 +0000

    refactor the factory for L8

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -1,26 +1,35 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
-use App\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Company;
 use Faker\Generator as Faker;
 
-$factory->define(Company::class, function (Faker $faker) {
-
-    // make folders 
-    if (!file_exists(storage_path('app/public/uploaded/logos'))) {
-        mkdir(storage_path('app/public/uploaded/logos'), 0755, true);
-    }
-
-    $companyName = $faker->company();
-
-    return [
-        'name' => $companyName,
-        'email' => $faker->unique()->companyEmail(),
-        'logo' => $faker->image(storage_path('app/public/uploaded/logos'), 100, 100, null, false, true, $companyName),
-        'website' => 'http://'.$faker->domainName(),
-        'created_by' => function() {
-            return factory(App\User::class)->create()->id;
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        // make folders 
+        if (!file_exists(storage_path('app/public/uploaded/logos'))) {
+            mkdir(storage_path('app/public/uploaded/logos'), 0755, true);
         }
-    ];
-});
+
+        $companyName = $this->faker->company();
+
+        return [
+            'name' => $companyName,
+            'email' => $this->faker->unique()->companyEmail(),
+            'logo' => $this->faker->image(storage_path('app/public/uploaded/logos'), 100, 100, null, false, true, $companyName),
+            'website' => 'http://'.$this->faker->domainName(),
+            'created_by' => function() {
+                return User::factory()->create()->id;
+            }
+        ];
+    }
+}
