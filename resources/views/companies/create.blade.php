f238e11978291b4d0a3b81209f08d40985da99fc
chiefofstack <chiefofstack@gmail.com>
2023-02-26 22:36:54 +0000

    complete the create company form and add success message above the card + add style for the top buttons

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -4,10 +4,33 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
+            @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible" >
+                    There were {{ $errors->count() }} errors found in the form.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+                            
+            <div class="card">
+                <div class="card-header">Create a Company</div>
                 <div class="card-body">
+                    <form action="{{ route('companies.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                        <div class="company-form">          
+                            <x-form.input name="name" label="Company Name"/>
+                            <x-form.input name="email" label="Email Address"/>
+                            <x-form.input name="logo" label="Company Logo"/>
+                            <x-form.input name="website" label="Website"/>
+                            <div class="row mb-3">                                    
+                                <div class="col-md-6 offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Add Company
+                                    </button>
+                                </div>
+                            </div>  
+                        </div>
+                    </form>   
                 </div>
             </div>
         </div>
