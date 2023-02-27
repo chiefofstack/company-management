a130f192593e3ccbff24899cffdb58c0b03783fc
chiefofstack <chiefofstack@gmail.com>
2023-02-27 18:01:53 +0000

    customized the  dashboard to be more presentable

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
+use App\Models\Employee;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $companyTotal = Company::where('created_by','=',auth()->user()->id)->count();
+        $employeeTotal = Employee::where('created_by','=',auth()->user()->id)->count();
+        return view('home',['companyTotal' => $companyTotal, 'employeeTotal' => $employeeTotal]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <h5 class="mt-2">{{ __('Dashboard') }}</h5>                    
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,10 +16,35 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
-                    <br>
-                    <a href="{{ route('companies.index') }}">View All Companies</a><br>
-                    <a href="{{ route('employees.index') }}">View All Employees</a>
+                    <div class="row">
+                        <p>Welcome {{ Auth::user()->name }}! </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    Companies
+                                </div>
+                                <div class="card-body">
+                                    <p>Here you can manage companies and see employees for each of them. Right now there's a total of {{ $companyTotal }} available for you. </p>
+                                    
+                                    <a href="{{ route('companies.index') }}" class="btn btn-primary">See All</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    Employees
+                                </div>
+                                <div class="card-body">
+                                    <p>This is where you see and manage all employees. You can still see which company an employee belongs to. Right now there's a total of {{ $employeeTotal }} available for you. </p>
+                                    
+                                    <a href="{{ route('employees.index') }}" class="btn btn-primary">See All</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     
                 </div>
             </div>
