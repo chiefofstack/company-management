e155486c8f307e0eaf4fc97d173f20eea98f1e09
chiefofstack <chiefofstack@gmail.com>
2023-02-27 08:26:07 +0000

    completed the employee create form

---
+++ resources/views/employees/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible" >
+                    There were {{ $errors->count() }} errors found in the form.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+                            
+            <div class="card">
+                <div class="card-header">Create an Employee</div>
+                <div class="card-body">
+                    <form action="{{ route('employees.store') }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                        <div class="company-form">          
+                            <x-form.input name="first_name" label="First Name"/>
+                            <x-form.input name="last_name" label="Last Name"/>
+                            <x-form.input name="company" label="Company"/>
+                            <x-form.input name="email" label="Email Address"/>
+                            <x-form.input name="phone_number" label="Phone Number"/>
+                            <div class="row mb-3">                                    
+                                <div class="col-md-6 offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Add Employee
+                                    </button>
+                                </div>
+                            </div>  
+                        </div>
+                    </form>   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
