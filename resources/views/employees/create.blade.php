c7572616d3d0a9c7a19aa6d64d17daa88c09f3bd
chiefofstack <chiefofstack@gmail.com>
2023-02-27 10:17:10 +0000

    complete the edit employee form + fix the company field on the create form

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -17,10 +17,10 @@
                 <div class="card-body">
                     <form action="{{ route('employees.store') }}" method="post" enctype="multipart/form-data">
                     @csrf
-                        <div class="company-form">          
+                        <div class="employee-form">          
                             <x-form.input name="first_name" label="First Name"/>
                             <x-form.input name="last_name" label="Last Name"/>
-                            <x-form.input name="company" label="Company"/>
+                            <x-form.input name="company_id" label="Company"/>
                             <x-form.input name="email" label="Email Address"/>
                             <x-form.input name="phone_number" label="Phone Number"/>
                             <div class="row mb-3">                                    
---
+++ resources/views/employees/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible" >
+                    There were {{ $errors->count() }} errors found in the form.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+                            
+            <div class="card">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h4>Edit Employee</h4>                    
+                    <div class="company-actions">
+                        <a href="{{ route('employees.index').'/'.$employee->id }}" type="button" class="btn btn-primary"><i class="far fa-eye"></i></a>
+                        <form method="POST" action="{{ route('employees.destroy', $employee) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" value="delete"><i class="far fa-trash-alt"></i></button>
+                        </form>
+                    </div>  
+                </div>
+                <div class="card-body">
+                   
+                    <form action="{{ route('employees.update', $employee) }}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @method('PATCH')
+                        <div class="employee-form">          
+                            <x-form.input name="first_name" label="First Name" :value="old('first_name', $employee->first_name)"/>
+                            <x-form.input name="last_name" label="Last Name" :value="old('last_name', $employee->last_name)"/>
+                            <x-form.input name="company_id" label="Company" :value="old('company_id', $employee->company->id)"/>
+                            <x-form.input name="email" label="Email Address" :value="old('email', $employee->email)"/>
+                            <x-form.input name="phone_number" label="Phone Number" :value="old('phone_number', $employee->phone_number)"/>
+                            <div class="row mb-3">                                    
+                                <div class="col-md-6 offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Update
+                                    </button>
+                                </div>
+                            </div>  
+                        </div>
+
+                    </form>   
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
